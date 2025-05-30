00264909a2509c86ceedb1bafe149dfbb1129a53
Sundaram Pandey <96278995+pandey-sundaram@users.noreply.github.com>
2025-05-31 00:10:50 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1748630463641

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__7db745e8._.css",
+  "static/chunks/node_modules_next_dist_c3a575e9._.js",
+  "static/chunks/src_components_Header_tsx_69d15ee6._.js"
+],
+    source: "dynamic"
+});
